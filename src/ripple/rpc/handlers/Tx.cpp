6d851313995dab188ab1e58be149947d41262676
CJ Cobb <ccobb@ripple.com>
2020-11-09 11:19:32 -0500

    [FOLD] fixes after rebase

--- src/ripple/rpc/handlers/Tx.cpp
+++ src/ripple/rpc/handlers/Tx.cpp
@@ -119,7 +119,7 @@ doTxPostgres(RPC::Context& context, TxArgs const& args)
     {
         auto start = std::chrono::system_clock::now();
         // The second argument of fetch is ignored when not using shards
-        if (auto obj = context.app.getNodeFamily().db().fetch(
+        if (auto obj = context.app.getNodeFamily().db().fetchNodeObject(
                 locator.getNodestoreHash(), locator.getLedgerSequence()))
         {
             auto node = SHAMapAbstractNode::makeFromPrefix(
